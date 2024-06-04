101e9eabbcc2a9eb2e55f0c86242170dc9349ac4
padget11 <padget.avery@capgemini.com>
2024-06-04 17:34:00 +0100

    fix unit test

--- test/unit/routes/non-ai.test.js
+++ test/unit/routes/non-ai.test.js
@@ -1,10 +1,16 @@
 const nonai = require('../../../app/routes/non-ai')
+jest.mock('convert-excel-to-json')
+jest.mock('../../../app/services/parser-service', () => ({
+    matchesBandM: jest.fn().mockResolvedValue(false),
+    matchesAsda: jest.fn().mockResolvedValue(false)
+  }));
 
 describe('/non-ai', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
   test('should return success', async () => {
+    jest.mock('convert-excel-to-json')
     const mockRequest = {}
     const mockH = {
       response: jest.fn(() => {
