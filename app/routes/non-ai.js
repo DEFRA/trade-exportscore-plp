1480eff98c09120338eb088610d7382a9d01d1f7
Padget Avery <padget.avery@capgemini.com>
2024-06-10 12:16:03 +0100

    fix lint

--- app/routes/non-ai.js
+++ app/routes/non-ai.js
@@ -4,7 +4,6 @@ const excelToJson = require('convert-excel-to-json')
 const parserService = require('../services/parser-service')
 
 const filename = plDir + 'test-packing-list.xlsx'
-
 let result = {}
 try {
   result = excelToJson({
@@ -12,18 +11,6 @@ try {
   })
 } catch (err) {
   console.log(err)
-
-
-var parsedPackingList;
-if (parserService.matchesAsda()) {
-    console.log('Packling list matches Asda')
-} else if (parserService.matchesBandM(result.Sheet1, filename)) {
-    console.log('Packling list matches BandM')
-	parsedPackingList = parserService.parseBandM(result.Sheet1)
-} else {
-	console.log('failed to parse')
-    parsedPackingList = parserService.failedParser()
->>>>>>> cd84b94 (add try catch for reading excel)
 }
 
 let parsedPackingList
