ae725961631ab6d1b570d6170140da0a0314b67c
padget11 <padget.avery@capgemini.com>
2024-06-10 15:06:18 +0100

    add b and m parser

---
+++ app/routes/non-ai.js
@@ -0,0 +1,31 @@
+const { plDir } = require('../config')
+
+const excelToJson = require('convert-excel-to-json')
+const parserService = require('../services/parser-service')
+
+const filename = plDir + 'test-packing-list.xlsx'
+let result = {}
+try {
+  result = excelToJson({
+    sourceFile: filename
+  })
+} catch (err) {
+  console.log(err)
+}
+
+let parsedPackingList
+if (parserService.matchesAsda()) {
+  console.log('Packling list matches Asda')
+} else if (parserService.matchesBandM(result.Sheet1, filename)) {
+  console.log('Packling list matches BandM')
+  parsedPackingList = parserService.parseBandM(result.Sheet1)
+} else {
+  console.log('failed to parse')
+  parsedPackingList = parserService.failedParser()
+}
+
+module.exports = {
+  method: 'GET',
+  path: '/non-ai',
+  handler: (_request, h) => h.response(parsedPackingList).code(200)
+}
