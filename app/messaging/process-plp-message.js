fb8ba0d2fa90f02fe7efb042a2e9c736fcb4c0b3
Padget Avery <padget.avery@capgemini.com>
2024-06-19 13:37:10 +0100

    add log

--- app/messaging/process-plp-message.js
+++ app/messaging/process-plp-message.js
@@ -1,7 +1,7 @@
 async function processPlpMessage (message, receiver) {
   try {
     await receiver.completeMessage(message)
-    console.log(message)
+    console.log(message.body)
   } catch (err) {
     console.error('Unable to process message:', err)
     await receiver.abandonMessage(message)
