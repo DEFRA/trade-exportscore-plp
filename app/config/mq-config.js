5ac889fe13a3611e7f76e87224e5eab20f95e3b5
Padget Avery <padget.avery@capgemini.com>
2024-07-25 15:03:58 +0100

    prettier lint

--- app/config/mq-config.js
+++ app/config/mq-config.js
@@ -1,35 +1,39 @@
 const mqConfig = {
   messageQueue: {
     host: process.env.MESSAGE_QUEUE_HOST,
-    useCredentialChain: process.env.NODE_ENV === 'production',
+    useCredentialChain: process.env.NODE_ENV === "production",
     managedIdentityClientId: process.env.AZURE_CLIENT_ID,
-    type: 'queue',
-    appInsights: process.env.NODE_ENV === 'production' ? require('applicationinsights') : undefined
+    type: "queue",
+    appInsights:
+      process.env.NODE_ENV === "production"
+        ? require("applicationinsights")
+        : undefined,
   },
   plpSubscription: {
     name: process.env.PLP_SUBSCRIPTION_NAME,
     address: process.env.PLP_SUBSCRIPTION_ADDRESS,
     topic: process.env.PLP_TOPIC_ADDRESS,
-    type: 'subscription'
+    type: "subscription",
   },
   parsedQueue: {
     name: process.env.PARSED_QUEUE_NAME,
     address: process.env.PARSED_QUEUE_ADDRESS,
     username: process.env.MESSAGE_QUEUE_USER,
-    password: process.env.MESSAGE_QUEUE_PASSWORD
-  }
-}
+    password: process.env.MESSAGE_QUEUE_PASSWORD,
+  },
+};
 
 const plpSubscription = {
   ...mqConfig.messageQueue,
-  ...mqConfig.plpSubscription
-}
+  ...mqConfig.plpSubscription,
+};
 
 const parsedQueue = {
   ...mqConfig.messageQueue,
-  ...mqConfig.parsedQueue
-}
+  ...mqConfig.parsedQueue,
+};
 
 module.exports = {
-  plpSubscription, parsedQueue
-}
+  plpSubscription,
+  parsedQueue,
+};
--- test/integration/narrow/routes/healthy.test.js
+++ test/integration/narrow/routes/healthy.test.js
@@ -1,51 +1,55 @@
-describe('Healthy test', () => {
-  let server
+describe("Healthy test", () => {
+  let server;
 
-  jest.mock('../../../../app/services/database-service')
-  const createServer = require('../../../../app/server')
-  const { sequelize } = require('../../../../app/services/database-service')
-  sequelize.authenticate = jest.fn()
+  jest.mock("../../../../app/services/database-service");
+  const createServer = require("../../../../app/server");
+  const { sequelize } = require("../../../../app/services/database-service");
+  sequelize.authenticate = jest.fn();
 
   beforeEach(async () => {
-    server = await createServer()
-    await server.initialize()
-  })
+    server = await createServer();
+    await server.initialize();
+  });
 
-  test('GET /healthy returns 200', async () => {
+  test("GET /healthy returns 200", async () => {
     const options = {
-      method: 'GET',
-      url: '/healthy'
-    }
-    sequelize.authenticate.mockReturnValue(true)
+      method: "GET",
+      url: "/healthy",
+    };
+    sequelize.authenticate.mockReturnValue(true);
 
-    const response = await server.inject(options)
+    const response = await server.inject(options);
 
-    expect(response.statusCode).toBe(200)
-  })
+    expect(response.statusCode).toBe(200);
+  });
 
-  test('GET /healthy returns 503 and error message if database check throws an error', async () => {
+  test("GET /healthy returns 503 and error message if database check throws an error", async () => {
     const options = {
-      method: 'GET',
-      url: '/healthy'
-    }
+      method: "GET",
+      url: "/healthy",
+    };
 
-    const errorMessage = 'database connection timeout'
-    sequelize.authenticate.mockImplementation(() => { throw new Error(errorMessage) })
+    const errorMessage = "database connection timeout";
+    sequelize.authenticate.mockImplementation(() => {
+      throw new Error(errorMessage);
+    });
 
-    const response = await server.inject(options)
+    const response = await server.inject(options);
 
-    expect(response.statusCode).toBe(503)
-    expect(response.payload).toBe(`Error running healthy check: ${errorMessage}`)
-  })
+    expect(response.statusCode).toBe(503);
+    expect(response.payload).toBe(
+      `Error running healthy check: ${errorMessage}`,
+    );
+  });
 
   afterEach(async () => {
-    await server.stop()
-    jest.clearAllMocks()
-  })
+    await server.stop();
+    jest.clearAllMocks();
+  });
 
   afterAll(async () => {
-    jest.resetAllMocks()
-    await sequelize.close()
-    await server.stop()
-  })
-})
+    jest.resetAllMocks();
+    await sequelize.close();
+    await server.stop();
+  });
+});
