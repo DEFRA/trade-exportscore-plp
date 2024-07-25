dc476e94f0459eeef032be94720be3e96dec92b0
Padget Avery <padget.avery@capgemini.com>
2024-07-25 13:38:45 +0100

    add int

---
+++ test/integration/narrow/index.test.js
@@ -0,0 +1,43 @@
+describe('Server tests', () => {
+  let createServer
+  let server
+
+  test('createServer returns server', async () => {
+    jest.mock('../../../app/config', () => {
+      return {
+        ...jest.requireActual('../../../app/config'),
+        port: 3004,
+        isDev: false
+      }
+    })
+    createServer = require('../../../app/server')
+    server = await createServer()
+    expect(server).toBeDefined()
+  })
+
+  test('createServer returns server in development', async () => {
+    jest.mock('../../../app/config', () => {
+      return {
+        ...jest.requireActual('../../../app/config'),
+        port: 3004,
+        isDev: true
+      }
+    })
+    createServer = require('../../../app/server')
+    server = await createServer()
+
+    expect(server).toBeDefined()
+  })
+
+  beforeEach(() => {
+    jest.resetModules()
+    jest.mock('../../../app/plugins/router', () => {
+      return {
+        plugin: {
+          name: 'mockrouter',
+          register: () => {}
+        }
+      }
+    })
+  })
+})
