15d329dbf2e5409da02aacbcf2741e966a1d807f
Padget Avery <padget.avery@capgemini.com>
2024-08-13 12:13:44 +0100

    db changes

--- app/models/packing-list.js
+++ app/models/packing-list.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       applicationId: { type: DataTypes.BIGINT, primaryKey: true },
       registrationApprovalNumber: DataTypes.STRING,
       allRequiredFieldsPresent: DataTypes.BOOLEAN,
+      parserModel: DataTypes.ENUM(modelEnums)
     },
     {
       freezeTableName: true,
@@ -14,7 +15,22 @@ module.exports = (sequelize, DataTypes) => {
     models.packingList.hasMany(models.item, {
       foreignKey: "applicationId",
       targetKey: "applicationId",
-    }); // should we use a separate id?
+    });
   };
   return PackingList;
 };
+
+modelEnums = {
+  TJMORRIS1: "tjmorris-1",
+  ASDA1: "asda-1",
+  ASDA2: "asda-2",
+  SAINSBURYS1: "sainsburys-1",
+  BANDM1: "bandm-1",
+  TESCO1: "tesco-1",
+  TESCO2: "tesco-2",
+  TESCO3: "tesco-3",
+  FOWLERWELCH1: "fowlerwelch-1",
+  NISA1: "nisa-1",
+  NISA2: "nisa-2",
+  BUFFALOAD1: "buffaload-1",
+}
