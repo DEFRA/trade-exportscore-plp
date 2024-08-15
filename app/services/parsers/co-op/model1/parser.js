750aafe5c5d549efa572890205195d8049a710bd
Jason Barden <jason.barden@capgemini.com>
2024-08-15 15:33:46 +0100

    add nature_of_products missed when fixing just now

--- app/services/parsers/co-op/model1/parser.js
+++ app/services/parsers/co-op/model1/parser.js
@@ -4,7 +4,7 @@ function parse(packingListJson) {
   const establishmentNumber = packingListJson[1].E ?? null;
   const packingListContents = packingListJson.slice(1).map((col) => ({
     description: col.O ?? null,
-    nature_of_products: null,
+    nature_of_products: col.L ?? null,
     commodity_code: col.P ?? null,
     number_of_packages: col.Q ?? null,
     total_net_weight_kg: col.S ?? null,
--- test/unit/services/parsers/co-op/model1/parser.test.js
+++ test/unit/services/parsers/co-op/model1/parser.test.js
@@ -5,6 +5,7 @@ describe("parseCoopModel1", () => {
     const packingListJson = [
       {
         E: "Dispatch RMS Establishment",
+        L: "Product Type/ Category",
         O: "Product/ Part Number description",
         P: "Tariff Code EU",
         Q: "Packages",
@@ -12,6 +13,7 @@ describe("parseCoopModel1", () => {
       },
       {
         E: "RMS-GB-000009-001",
+        L: "Salad",
         O: "Co-op Red Peppers Each",
         P: "0709601000",
         Q: 12,
@@ -19,6 +21,7 @@ describe("parseCoopModel1", () => {
       },
       {
         E: "RMS-GB-000009-001",
+        L: "Salad",
         O: "Co-op Ripe And Ready To Eat Avocados 2S.",
         P: "0709601001",
         Q: 1,
@@ -32,6 +35,8 @@ describe("parseCoopModel1", () => {
     expect(result.items).toHaveLength(2);
     expect(result.items[0].description).toBe(packingListJson[1].O);
     expect(result.items[1].description).toBe(packingListJson[2].O);
+    expect(result.items[0].nature_of_products).toBe(packingListJson[1].L);
+    expect(result.items[1].nature_of_products).toBe(packingListJson[2].L);
     expect(result.items[0].commodity_code).toBe(packingListJson[1].P);
     expect(result.items[1].commodity_code).toBe(packingListJson[2].P);
     expect(result.items[0].number_of_packages).toBe(packingListJson[1].Q);
