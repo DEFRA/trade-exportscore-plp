73cd38393e4619256b56e743bf730bf1bce1c96e
Jason Barden <jason.barden@capgemini.com>
2024-08-15 15:22:49 +0100

    Fix commodity code mis-match error

--- app/services/parsers/co-op/model1/parser.js
+++ app/services/parsers/co-op/model1/parser.js
@@ -4,8 +4,8 @@ function parse(packingListJson) {
   const establishmentNumber = packingListJson[1].E ?? null;
   const packingListContents = packingListJson.slice(1).map((col) => ({
     description: col.O ?? null,
-    nature_of_products: col.P ?? null,
-    commodity_code: null,
+    nature_of_products: null,
+    commodity_code: col.P ?? null,
     number_of_packages: col.Q ?? null,
     total_net_weight_kg: col.S ?? null,
   }));
