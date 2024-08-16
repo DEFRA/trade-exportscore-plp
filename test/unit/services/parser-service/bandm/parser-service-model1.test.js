3a1adfea1659026ed98963abf382faf7fb2357f5
Jason Barden <jason.barden@capgemini.com>
2024-08-16 17:17:24 +0100

    Add test data and additional test to B and M Model 1

--- test/unit/services/parser-service/bandm/parser-service-model1.test.js
+++ test/unit/services/parser-service/bandm/parser-service-model1.test.js
@@ -1,49 +1,24 @@
-const ParserService = require("../../../../../app/services/parser-service");
+const parserService = require("../../../../../app/services/parser-service");
+const model = require("../../../test-helpers/bandm/model1/data-model");
 
-const filename = "packinglist.xlsx";
-const packingListJson = {
-  Sheet1: [
-    {},
-    {},
-    {
-      H: "WAREHOUSE SCHEME NUMBER:",
-      I: "RMS-GB-000005-001",
-    },
-    {},
-    {},
-    {
-      A: "PRODUCT CODE (SHORT)",
-      B: "PRISM",
-      C: "ITEM DESCRIPTION",
-      D: "COMMODITY CODE",
-      E: "PLACE OF DISPATCH",
-      F: "TOTAL NUMBER OF CASES",
-      G: "NET WEIGHT",
-      H: "GROSS WEIGHT",
-      I: "ANIMAL ORIGIN",
-    },
-  ],
-};
+const filename = "packinglist-bandm-model1.xlsx";
 
 describe("matchesBAndMModel1", () => {
   test("returns isParsed as true", () => {
-    const result = ParserService.findParser(packingListJson, filename);
+    const result = parserService.findParser(model.validModel, filename);
 
     expect(result.isParsed).toBeTruthy();
   });
 
-  test("matches valid BAndM Model 1 file and calls parser", () => {
-    const result = ParserService.findParser(packingListJson, filename);
+  test("matches valid BAndM Model 1 file, calls parser and returns all_required_fields_present as true", () => {
+    const result = parserService.findParser(model.validModel, filename);
 
-    expect(result).toEqual({
-      isParsed: true,
-      packingList: {
-        business_checks: {
-          all_required_fields_present: true,
-        },
-        items: [],
-        registration_approval_number: "RMS-GB-000005-001",
-      },
-    });
+    expect(result).toEqual(model.validTestResult);
+  });
+
+  test("matches valid BAndM Model 1 file, calls parser, but returns all_required_fields_present as false when cells missing", () => {
+    const result = parserService.findParser(model.invalidModel_MissingColumnCells, filename);
+
+    expect(result).toEqual(model.invalidTestResult_MissingCells);
   });
 });
