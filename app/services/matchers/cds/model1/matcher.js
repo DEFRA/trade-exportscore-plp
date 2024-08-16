88f629dcbf6c5caea1c4358700b6deb65d265983
David Pizzi <81577860+david-pizzi-cg@users.noreply.github.com>
2024-08-16 11:32:14 +0100

    AB#426520 : CDS parser model 1 (#42)

---
+++ app/services/matchers/cds/model1/matcher.js
@@ -0,0 +1,55 @@
+const MatcherResult = require("../../../matches-result");
+const FileExtension = require("../../../../utilities/file-extension");
+
+function matches(packingList, filename) {
+  const establishmentNumberRow = 1;
+  try {
+    if (FileExtension.matches(filename, "xlsx") !== MatcherResult.CORRECT) {
+      return MatcherResult.WRONG_EXTENSIONS;
+    }
+
+    const sheet = Object.keys(packingList)[0];
+    // check for correct establishment number
+    const placeOfDispatchSplit =
+      packingList[sheet][establishmentNumberRow].K?.split("/") ?? [];
+    const establishmentNumber =
+      placeOfDispatchSplit.length > 1 ? placeOfDispatchSplit[1].trim() : null;
+
+    const regex = /^RMS-GB-000252-\d{3}$/;
+    if (!regex.test(establishmentNumber)) {
+      return MatcherResult.WRONG_ESTABLISHMENT_NUMBER;
+    }
+
+    // check for header values
+    const header = {
+      A: "TruckID",
+      B: "Dept",
+      C: "SubDept",
+      D: "Product",
+      E: "# Packages",
+      F: "# Units",
+      G: "GrossWeight",
+      H: "NetWeight",
+      I: "NatureOfProduct",
+      J: "Treatment",
+      K: "PlaceOfDispatch",
+    };
+
+    for (const key in header) {
+      if (
+        !packingList[sheet][0] ||
+        packingList[sheet][0][key] !== header[key]
+      ) {
+        return MatcherResult.WRONG_HEADER;
+      }
+    }
+
+    return MatcherResult.CORRECT;
+  } catch (err) {
+    return MatcherResult.GENERIC_ERROR;
+  }
+}
+
+module.exports = {
+  matches,
+};
