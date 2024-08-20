7157acec4e04ce5f3ea53fa815913388312d30e4
david-pizzi-cg <david.pizzi@capgemini.com>
2024-08-20 14:54:45 +0100

    Add utility function to remove trailing spaces and empty strings

---
+++ test/unit/utilities/json-file.test.js
@@ -0,0 +1,40 @@
+const JsonFile = require("../../../app/utilities/json-file");
+
+describe("json-checks", () => {
+  test("should replace empty string values with null", () => {
+    const input = '{"name": "", "age": "25", "address": " " }';
+    const expectedOutput = '{"name":null,"age":"25","address":null}';
+    const result = JsonFile.sanitises(input);
+    expect(result).toBe(expectedOutput);
+  });
+
+  test("should trim trailing and leading whitespaces from non-empty string values", () => {
+    const input = '{"name": " John Doe ", "age": " 30 " }';
+    const expectedOutput = '{"name":"John Doe","age":"30"}';
+    const result = JsonFile.sanitises(input);
+    expect(result).toBe(expectedOutput);
+  });
+
+  test("should handle nested objects and arrays", () => {
+    const input =
+      '{"user": {"name": " Jane Doe ", "nickname": ""}, "tags": ["  tag1 ", "  "]}';
+    const expectedOutput =
+      '{"user":{"name":"Jane Doe","nickname":null},"tags":["tag1",null]}';
+    const result = JsonFile.sanitises(input);
+    expect(result).toBe(expectedOutput);
+  });
+
+  test("should return null for invalid JSON string", () => {
+    const input = '{"name": "John Doe", "age": 30'; // Invalid JSON
+    const result = JsonFile.sanitises(input);
+    expect(result).toBeNull();
+  });
+
+  test("should leave non-string values unchanged", () => {
+    const input = '{"age": 25, "isActive": true, "details": {"height": 180}}';
+    const expectedOutput =
+      '{"age":25,"isActive":true,"details":{"height":180}}';
+    const result = JsonFile.sanitises(input);
+    expect(result).toBe(expectedOutput);
+  });
+});
