105e0bb578b5207afdf57ca3c77535c87e15d330
Padget Avery <padget.avery@capgemini.com>
2024-08-23 16:46:35 +0100

    add test

--- test/unit/messaging/send-parsed-message.test.js
+++ test/unit/messaging/send-parsed-message.test.js
@@ -1,13 +1,20 @@
-const { sendParsedAdp } = require("../../../app/messaging/send-parsed-message");
+const {
+  sendParsedAdp,
+  sendParsed,
+} = require("../../../app/messaging/send-parsed-message");
 const config = require("../../../app/config");
 const { MessageSender } = require("adp-messaging");
 const createMessage = require("../../../app/messaging/create-message");
+const { ServiceBusClient } = require("@azure/service-bus");
+const { DefaultAzureCredential } = require("@azure/identity");
 
 jest.mock("../../../app/config");
 jest.mock("adp-messaging");
 jest.mock("../../../app/messaging/create-message");
+jest.mock("@azure/service-bus");
+jest.mock("@azure/identity");
 
-describe("sendParsed", () => {
+describe("sendParsedAdp", () => {
   test("should send a message", async () => {
     // Arrange
     const message = {};
@@ -24,5 +31,20 @@ describe("sendParsed", () => {
     expect(MessageSender).toHaveBeenCalledWith(config.tpQueue);
     expect(sendMessage).toHaveBeenCalledWith(message);
     expect(closeConnection).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe("sendParsed", () => {
+  test("should send message", async () => {
+    // arrange
+    const message = {};
+    createMessage.mockReturnValue(message);
+    // act
+    await sendParsed(true, "123");
+
+    //assert
+    expect(createMessage).toHaveBeenCalledWith(true, "123");
+    expect(DefaultAzureCredential).toHaveBeenCalledTimes(1);
+    expect(ServiceBusClient).toHaveBeenCalledTimes(1);
   });
 });
