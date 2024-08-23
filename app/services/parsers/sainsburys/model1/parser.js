6775222a814f91af2bdec6bd4970639b59eebbf1
Jason Barden <jason.barden@capgemini.com>
2024-08-23 11:44:17 +0100

    Remove debugging statements from Sainsburys parser test

--- app/services/parsers/sainsburys/model1/parser.js
+++ app/services/parsers/sainsburys/model1/parser.js
@@ -2,10 +2,8 @@ const CombineParser = require("../../../parser-combine");
 const ParserModel = require("../../../parser-model");
 
 function parse(packingListJson) {
-  console.log(packingListJson);
   const establishmentNumber =
     packingListJson[1].N?.replace(/\u200B/g, "") ?? null;
-  console.log("establishmentNumber: ", establishmentNumber);
   const packingListContents = packingListJson.slice(1).map((col) => ({
     description: col.E ?? null,
     nature_of_products: col.C ?? null,
@@ -15,7 +13,6 @@ function parse(packingListJson) {
     total_net_weight_kg: col.H ?? null,
   }));
 
-  console.log("packingListContents: ", packingListContents);
   return CombineParser.combine(
     establishmentNumber,
     packingListContents,
