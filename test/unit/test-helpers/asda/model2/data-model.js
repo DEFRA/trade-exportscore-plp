fd4fa06e47b8018ddbbe6aa0f812ddbd33857649
Jason Barden <jason.barden@capgemini.com>
2024-08-23 17:07:05 +0100

    Update ASDA parsers to use shared data-model

--- test/unit/test-helpers/asda/model2/data-model.js
+++ test/unit/test-helpers/asda/model2/data-model.js
@@ -129,4 +129,21 @@ module.exports = {
     registration_approval_number: "RMS-GB-000015-010",
     parserModel: ParserModel.ASDA2,
   },
+  emptyTestResult: {
+    business_checks: {
+      all_required_fields_present: true,
+    },
+    items: [
+      {
+        commodity_code: null,
+        description: null,
+        nature_of_products: null,
+        number_of_packages: null,
+        total_net_weight_kg: null,
+        type_of_treatment: null,
+      },
+    ],
+    registration_approval_number: null,
+    parserModel: ParserModel.ASDA2,
+  },
 };
