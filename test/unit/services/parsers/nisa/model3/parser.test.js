8bd9724e9742e83d419740509533c2f340a3be40
Jason Barden <jason.barden@capgemini.com>
2024-08-23 21:01:08 +0100

    Update Nisa2 & 3 to use the shared data-model

--- test/unit/services/parsers/nisa/model3/parser.test.js
+++ test/unit/services/parsers/nisa/model3/parser.test.js
@@ -1,80 +1,16 @@
-const Parser = require("../../../../../../app/services/parsers/nisa/model3/parser");
-const ParserModel = require("../../../../../../app/services/parser-model");
+const parser = require("../../../../../../app/services/parsers/nisa/model3/parser");
+const model = require("../../../../test-helpers/nisa/model3/data-model");
 
 describe("parseNisa3", () => {
-  test("parses valid json", () => {
-    const packingListJson = [
-      {
-        A: "RMS ESTABLISHMENT NO",
-      },
-      {
-        A: "RMS-GB-000025-003",
-      },
-      {
-        C: "PRODUCT TYPE CATEGORY",
-        E: "PART NUMBER DESCRIPTION",
-        F: "TARIFF CODE EU",
-        G: "PACKAGES",
-        I: "NET WEIGHT TOTAL",
-      },
-      {
-        C: "PRODUCT_TYPE_CATEGORY675 - CHEESE - C",
-        E: "DAIRYLEA DUNKERS JUMBO PM80P",
-        F: "2005995090",
-        G: 2,
-        I: 2.5,
-      },
-      {
-        C: "900 - VEGETABLES PREPACK-C",
-        E: "NISA BROCCOLI",
-        F: "0403209300",
-        G: 1,
-        I: 2,
-      },
-    ];
+  test("parses populated json", () => {
+    const result = parser.parse(model.validModel.sheet);
 
-    const result = Parser.parse(packingListJson);
-
-    expect(result.registration_approval_number).toBe(packingListJson[1].A);
-    expect(result.items).toHaveLength(2);
-    expect(result.items[0].description).toBe(packingListJson[3].E);
-    expect(result.items[1].description).toBe(packingListJson[4].E);
-    expect(result.items[0].nature_of_products).toBe(packingListJson[3].C);
-    expect(result.items[1].nature_of_products).toBe(packingListJson[4].C);
-    expect(result.items[0].commodity_code).toBe(packingListJson[3].F);
-    expect(result.items[1].commodity_code).toBe(packingListJson[4].F);
-    expect(result.items[0].number_of_packages).toBe(packingListJson[3].G);
-    expect(result.items[1].number_of_packages).toBe(packingListJson[4].G);
-    expect(result.items[0].total_net_weight_kg).toBe(packingListJson[3].I);
-    expect(result.items[1].total_net_weight_kg).toBe(packingListJson[4].I);
-    expect(result.parserModel).toBe(ParserModel.NISA3);
+    expect(result).toEqual(model.validTestResult);
   });
 
   test("parses empty json", () => {
-    const packingListJson = [
-      {
-        A: "RMS_ESTABLISHMENT_NO",
-      },
-      {},
-      {
-        C: "PRODUCT TYPE CATEGORY",
-        E: "PART NUMBER DESCRIPTION",
-        F: "TARIFF CODE EU",
-        G: "PACKAGES",
-        I: "NET WEIGHT TOTAL",
-      },
-      {},
-    ];
+    const result = parser.parse(model.emptyModel.sheet);
 
-    const result = Parser.parse(packingListJson);
-
-    expect(result.registration_approval_number).toBeNull();
-    expect(result.items).toHaveLength(1);
-    expect(result.items[0].description).toBeNull();
-    expect(result.items[0].nature_of_products).toBeNull();
-    expect(result.items[0].commodity_code).toBeNull();
-    expect(result.items[0].number_of_packages).toBeNull();
-    expect(result.items[0].total_net_weight_kg).toBeNull();
-    expect(result.parserModel).toBe(ParserModel.NISA3);
+    expect(result).toEqual(model.emptyTestResult);
   });
 });
