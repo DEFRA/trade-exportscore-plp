448d0c5adbb69ad5b88cfbacf914c3ffc3173312
Padget Avery <padget.avery@capgemini.com>
2024-09-02 11:11:49 +0100

    edit message format

--- app/messaging/create-message.js
+++ app/messaging/create-message.js
@@ -10,14 +10,16 @@ function createMessage(parsedResult, applicationId) {
     source: "trade-exportscore-plp",
     MessageId: v4(),
     CorrelationId: v4(),
-    ContentType: "application/json",
-    EntityKey: applicationId,
-    PublisherId: "PLP",
-    SchemaVersion: 1,
-    Type: "Internal",
-    Status: "Complete",
-    TimestampUtc: Date.now(),
     Label: "plp.idcoms.parsed",
+    applicationProperties: {
+      ContentType: "application/json",
+      EntityKey: applicationId,
+      PublisherId: "PLP",
+      SchemaVersion: 1,
+      Type: "Internal",
+      Status: "Complete",
+      TimestampUtc: Date.now(),
+    }
   };
 }
 
