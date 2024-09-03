1a77e58d9310cff90bc699d206118db749751c22
Padget Avery <padget.avery@capgemini.com>
2024-09-03 12:32:45 +0100

    refactor tests

--- test/unit/services/matchers/warrens/model1/matcher.test.js
+++ test/unit/services/matchers/warrens/model1/matcher.test.js
@@ -1,5 +1,6 @@
 const MatcherResult = require("../../../../../../app/services/matches-result");
 const warrensMatcher = require("../../../../../../app/services/matchers/warrens/model1/matcher");
+const model = require("../../../../test-helpers/warrens/model1/data-model");
 
 describe("matchesWarrens1", () => {
   test("returns generic error for empty json", () => {
@@ -17,150 +18,50 @@ describe("matchesWarrens1", () => {
   });
 
   test("returns wrong establishment number for missing establishment number for one sheet", () => {
-    const packingListJson = {
-      sheet: [
-        {
-          F: "Description of goods",
-        },
-        {
-          M: "Incorrect",
-        },
-      ],
-    };
     const filename = "packinglist.xlsx";
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(
+      model.invalid_Model_IncorrectEstablishmentNumber,
+      filename,
+    );
     expect(result).toBe(MatcherResult.WRONG_ESTABLISHMENT_NUMBER);
   });
 
   test("returns wrong establishment number for missing establishment numbers of multiple sheets", () => {
-    const packingListJson = {
-      sheet: [
-        {
-          C: "Commodity code",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-123",
-        },
-      ],
-      sheet2: [
-        {
-          F: "Description of goods",
-        },
-        {
-          M: "Incorrect",
-        },
-      ],
-    };
     const filename = "packinglist.xlsx";
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(
+      model.invalid_Model_IncorrectEstablishmentNumberMultiple,
+      filename,
+    );
     expect(result).toBe(MatcherResult.WRONG_ESTABLISHMENT_NUMBER);
   });
 
   test("returns wrong header for incorrect header values of one sheet", () => {
     const filename = "packinglist.xlsx";
-    const packingListJson = {
-      sheet: [
-        {
-          C: "Incorrect",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-    };
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(
+      model.invalid_Model_IncorrectHeader,
+      filename,
+    );
     expect(result).toBe(MatcherResult.WRONG_HEADER);
   });
 
   test("returns wrong header for incorrect header values of multiple sheets", () => {
     const filename = "packinglist.xlsx";
-    const packingListJson = {
-      sheet: [
-        {
-          C: "Commodity code",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-      sheet2: [
-        {
-          C: "Incorrect",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-    };
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(
+      model.invalid_Model_IncorrectHeaderMultiple,
+      filename,
+    );
     expect(result).toBe(MatcherResult.WRONG_HEADER);
   });
 
   test("returns correct for correct headers for one sheet", () => {
     const filename = "packinglist.xlsx";
-    const packingListJson = {
-      sheet: [
-        {
-          C: "Commodity code",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-    };
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(model.validModel, filename);
     expect(result).toBe(MatcherResult.CORRECT);
   });
 
   test("returns correct for correct headers of multiple sheets", () => {
     const filename = "packinglist.xlsx";
-    const packingListJson = {
-      sheet: [
-        {
-          C: "Commodity code",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-      sheet2: [
-        {
-          C: "Commodity code",
-          F: "Description of goods",
-          H: "No. of pkgs",
-          K: "Item Net Weight (kgs)",
-          N: "Treatment Type (Chilled /Ambient)",
-        },
-        {
-          M: "RMS-GB-000174-002",
-        },
-      ],
-    };
-    const result = warrensMatcher.matches(packingListJson, filename);
+    const result = warrensMatcher.matches(model.validModel_Multiple, filename);
     expect(result).toBe(MatcherResult.CORRECT);
   });
 });
