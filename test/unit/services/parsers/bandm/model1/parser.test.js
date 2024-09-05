3bb2c0cd43b7e6aafa3a0c8c44fc5ba222f1f338
Jason Barden <jason.barden@capgemini.com>
2024-09-05 15:54:26 +0100

    Refactor tests to remove the need for the resetEmpty

--- test/unit/services/parsers/bandm/model1/parser.test.js
+++ test/unit/services/parsers/bandm/model1/parser.test.js
@@ -1,15 +1,24 @@
 const parser = require("../../../../../../app/services/parsers/bandm/model1/parser");
 const model = require("../../../../test-helpers/bandm/model1/data-model");
+const JsonFile = require("../../../../../../app/utilities/json-file");
 
 describe("parseBandMModel1", () => {
   test("parses populated json", () => {
-    const result = parser.parse(model.validModel.Sheet1);
+    const packingListJson = JSON.stringify(model.validModel.Sheet1);
+    const sanitisedPackingListJson = JsonFile.sanitises(packingListJson);
+    const sanitisedPackingList = JSON.parse(sanitisedPackingListJson);
+
+    const result = parser.parse(sanitisedPackingList);
 
     expect(result).toEqual(model.validTestResult);
   });
 
   test("parses empty json", () => {
-    const result = parser.parse(model.emptyModel.Sheet1);
+    const packingListJson = JSON.stringify(model.emptyModel.Sheet1);
+    const sanitisedPackingListJson = JsonFile.sanitises(packingListJson);
+    const sanitisedPackingList = JSON.parse(sanitisedPackingListJson);
+
+    const result = parser.parse(sanitisedPackingList);
 
     expect(result).toEqual(model.emptyTestResult);
   });
