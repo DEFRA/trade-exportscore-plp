8fc8c8aa082c7b7029053b72eae5e5b651474b64
Jason Barden <jason.barden@capgemini.com>
2024-09-11 12:02:28 +0100

    move nutrica and sainsbury's matchers

--- test/unit/services/matchers/nutricia/model1/matcher.test.js
+++ test/unit/services/matchers/nutricia/model1/matcher.test.js
@@ -1,4 +1,4 @@
-const Matcher = require("../../../../../../app/services/matchers/nutricia/model1/model1");
+const Matcher = require("../../../../../../app/services/matchers/nutricia/model1");
 const MatcherResult = require("../../../../../../app/services/matcher-result");
 const model = require("../../../../test-helpers/nutricia/model1/data-model");
 
