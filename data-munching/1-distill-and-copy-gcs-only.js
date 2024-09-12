f37bc355065f9b075f74d8a81925dc3080814c78
Jim Davenport <jim@dynamicmonkeys.com>
2024-09-12 18:14:15 +0100

    Moved deps & reworked applicationIDs to include RMS numbers

--- data-munching/1-distill-and-copy-gcs-only.js
+++ data-munching/1-distill-and-copy-gcs-only.js
@@ -1,15 +1,28 @@
 const fs = require('fs/promises')
 const path = require('path')
-const { ids: applicationIds } = require('./application-ids')
-const { moveDirectory } = require('./move-dirs')
+const excelToJson = require('@boterop/convert-excel-to-json')
 
-console.log('Running matching')
-console.log('Application IDs:', applicationIds.length)
+const { moveDirectory } = require('./dependencies/move-dirs')
 
-const directoryPath = path.join('/Path', 'to', 'dump')
+const directoryPath = path.join('/Volumes', 'T7 Shield', 'MEGA DUMP')
+
+let applicationsMatrix = []
 let matchingDirectories = []
 
-async function listFiles () {
+console.log('Running matching')
+
+try {
+  applicationsMatrix = excelToJson({ sourceFile: './general-certificates-matrix.csv' }).Sheet1
+  console.log('Num of application IDs:', applicationsMatrix.length)
+  const applicationIds = applicationsMatrix.map(row => row.A)
+  console.log('application ids:', applicationIds)
+
+  listFiles(applicationIds)
+} catch (err) {
+  console.error(err)
+}
+
+async function listFiles (gcApplications) {
   try {
     const directories = await fs.readdir(directoryPath, { withFileTypes: true })
     console.log('Directories counter:', directories.length)
@@ -17,7 +30,7 @@ async function listFiles () {
     matchingDirectories = directories
       .filter(file => {
         const dirName = file.name.toString()
-        const isMatch = applicationIds.map(id => id.toString()).includes(dirName)
+        const isMatch = gcApplications.map(id => id.toString()).includes(dirName)
         return isMatch
       })
 
@@ -27,11 +40,8 @@ async function listFiles () {
       const destDir = './general-certificates/' + dir.name + '/'
       moveDirectory(srcDir, destDir)
     })
-
     console.log('Matched dirs:', matchingDirectories.length)
   } catch (err) {
     console.log('Unable to scan directory: ' + err)
   }
 }
-
-listFiles()
--- data-munching/README.md
+++ data-munching/README.md
@@ -1,8 +1,10 @@
 # How to use data munching
 
 ## Start here
-You'll need a dump of the data in the applicationID-per-dir structure plus a dump of the application IDs from CRM.
+You'll need a dump of the data in the applicationID-per-dir structure plus a dump of the application IDs+RMS IDs from CRM.
 
 ## 1-distill-and-copy-gcs-only
 This builds up a list of dirs from the mega dump and matches against application IDs from CRM. It then takes this array and copies the data to a separate dir. This makes a dir full of just GC-related data to cut down the amount of parsing needed.
 
+## 2
+Updated applicationID lookup to include RMS numbers.
