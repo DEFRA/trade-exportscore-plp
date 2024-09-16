354ed89ca0b44786f462f7635e78d904b77a2ebd
Jason Barden <jason.barden@capgemini.com>
2024-09-16 13:46:16 +0100

    add basic startup logging and error capture in server.js

--- app/server.js
+++ app/server.js
@@ -4,40 +4,45 @@ const { sequelize } = require("./services/database-service");
 const messageService = require("./messaging");
 
 async function createServer() {
-  await sequelize.authenticate();
+  console.log("Running the createServer method");
+  try {
+    await sequelize.authenticate();
 
-  // Create the hapi server
-  const server = hapi.server({
-    port: config.port,
-    routes: {
-      validate: {
-        options: {
-          abortEarly: false,
+    // Create the hapi server
+    const server = hapi.server({
+      port: config.port,
+      routes: {
+        validate: {
+          options: {
+            abortEarly: false,
+          },
         },
       },
-    },
-  });
+    });
 
-  // Register the plugins
-  await server.register(require("./plugins/router"));
+    // Register the plugins
+    await server.register(require("./plugins/router"));
 
-  if (config.isDev) {
-    await server.register(require("blipp"));
+    if (config.isDev) {
+      await server.register(require("blipp"));
+    }
+
+    await messageService.start();
+
+    process.on("SIGTERM", async function () {
+      await messageService.stop();
+      process.exit(0);
+    });
+
+    process.on("SIGINT", async function () {
+      await messageService.stop();
+      process.exit(0);
+    });
+
+    return server;
+  } catch (error) {
+    console.error("createServer encountered an error: ", error);
   }
-
-  await messageService.start();
-
-  process.on("SIGTERM", async function () {
-    await messageService.stop();
-    process.exit(0);
-  });
-
-  process.on("SIGINT", async function () {
-    await messageService.stop();
-    process.exit(0);
-  });
-
-  return server;
 }
 
 module.exports = createServer;
