24753e40c8b707d6ed618782e89fc43d8bf239fe
Padget Avery <padget.avery@capgemini.com>
2024-10-03 17:24:19 +0100

    rework tests

--- test/unit/services/matchers/iceland/model1.test.js
+++ test/unit/services/matchers/iceland/model1.test.js
@@ -1,6 +1,6 @@
 const matcher = require("../../../../../app/services/matchers/iceland/model1");
 const matcher_result = require("../../../../../app/services/matcher-result");
-const model = require("../../../test-data-and-results/models/iceland/model1")
+const model = require("../../../test-data-and-results/models/iceland/model1");
 
 jest.mock("../../../../../app/services/document-intelligence");
 
@@ -18,19 +18,22 @@ describe("matchesIceland", () => {
   test.each([
     [matcher_result.CORRECT, model.validModel],
     [MatcherResult.GENERIC_ERROR, {}],
-    [matcher_result.WRONG_ESTABLISHMENT_NUMBER, model.invalidModel_WrongRemosNumber],
-    [matcher_result.WRONG_ESTABLISHMENT_NUMBER, model.invalidModel_MissingRemosElement],
-  ])(
-    "returns '%s' for '%s'",
-    async (expected, inputModel) => {
-      const filename = "PackingList.xlsx";
-      runAnalysis.mockImplementationOnce(() => {
-        return inputModel;
-      });
-  
-      const result = await matcher.matches("", filename);
-  
-      expect(result.isMatched).toEqual(expected);
-    } 
-  );
+    [
+      matcher_result.WRONG_ESTABLISHMENT_NUMBER,
+      model.invalidModel_WrongRemosNumber,
+    ],
+    [
+      matcher_result.WRONG_ESTABLISHMENT_NUMBER,
+      model.invalidModel_MissingRemosElement,
+    ],
+  ])("returns '%s' for iceland model", async (expected, inputModel) => {
+    const filename = "PackingList.xlsx";
+    runAnalysis.mockImplementationOnce(() => {
+      return inputModel;
+    });
+
+    const result = await matcher.matches("", filename);
+
+    expect(result.isMatched).toEqual(expected);
+  });
 });
