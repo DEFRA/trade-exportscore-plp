cdf0dace8075b89c2662f2c760a2e45cfc4254d0
Padget Avery <padget.avery@capgemini.com>
2024-10-03 17:04:08 +0100

    add model tests

--- test/unit/services/matchers/iceland/model1.test.js
+++ test/unit/services/matchers/iceland/model1.test.js
@@ -1,6 +1,6 @@
 const matcher = require("../../../../../app/services/matchers/iceland/model1");
 const matcher_result = require("../../../../../app/services/matcher-result");
-const model = require("../../../test-data-and-results/models/")
+const model = require("../../../test-data-and-results/models/iceland/model1")
 
 jest.mock("../../../../../app/services/document-intelligence");
 
@@ -8,71 +8,29 @@ const {
   createDocumentIntelligenceClient,
   runAnalysis,
 } = require("../../../../../app/services/document-intelligence");
+const MatcherResult = require("../../../../../app/services/matcher-result");
 
 createDocumentIntelligenceClient.mockImplementation(() => {
   return jest.fn();
 });
 
 describe("matchesIceland", () => {
-  test("returns Correct", async () => {
-    const filename = "PackingList.xlsx";
-    runAnalysis.mockImplementationOnce(() => {
-      return {
-        fields: {
-          PartialNIRMSNumber: {
-            content: "KingdomRMS_ESTABLISHMENT_NORMS-GB-000040-",
-          },
-        },
-      };
-    });
-
-    const result = await matcher.matches("", filename);
-
-    expect(result.isMatched).toEqual(matcher_result.CORRECT);
-  });
-
-  test("return generic error for empty document", async () => {
-    const filename = "PackingList.xlsx";
-    runAnalysis.mockImplementationOnce(() => {
-      return {};
-    });
-
-    const result = await matcher.matches("", filename);
-
-    expect(result.isMatched).toEqual(matcher_result.GENERIC_ERROR);
-  });
-
-  test("returns wrong establishment number for wrong establishment number", async () => {
-    const filename = "PackingList.xlsx";
-    runAnalysis.mockImplementationOnce(() => {
-      return {
-        fields: {
-          PartialNIRMSNumber: {
-            content: "KingdomRMS_ESTABLISHMENT_NORMS-GB-000041-",
-          },
-        },
-      };
-    });
-
-    const result = await matcher.matches("", filename);
-
-    expect(result.isMatched).toEqual(matcher_result.WRONG_ESTABLISHMENT_NUMBER);
-  });
-
-  test("returns wrong establishment number for missing element", async () => {
-    const filename = "PackingList.xlsx";
-    runAnalysis.mockImplementationOnce(() => {
-      return {
-        fields: {
-          remosNumber: {
-            content: "KingdomRMS_ESTABLISHMENT_NORMS-GB-000041-",
-          },
-        },
-      };
-    });
-
-    const result = await matcher.matches("", filename);
-
-    expect(result.isMatched).toEqual(matcher_result.WRONG_ESTABLISHMENT_NUMBER);
-  });
+  test.each([
+    [matcher_result.CORRECT, model.validModel],
+    [MatcherResult.GENERIC_ERROR, {}],
+    [matcher_result.WRONG_ESTABLISHMENT_NUMBER, model.invalidModel_WrongRemosNumber],
+    [matcher_result.WRONG_ESTABLISHMENT_NUMBER, model.invalidModel_MissingRemosElement],
+  ])(
+    "returns '%s' for '%s'",
+    async (expected, inputModel) => {
+      const filename = "PackingList.xlsx";
+      runAnalysis.mockImplementationOnce(() => {
+        return inputModel;
+      });
+  
+      const result = await matcher.matches("", filename);
+  
+      expect(result.isMatched).toEqual(expected);
+    } 
+  );
 });
