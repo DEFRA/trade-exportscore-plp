6fc0591c07039c118ddb8f436952a9da4d08d1b3
Padget Avery <padget.avery@capgemini.com>
2024-10-11 16:35:52 +0100

    add randon int utulity

--- app/routes/ai.js
+++ app/routes/ai.js
@@ -5,14 +5,7 @@ const { findParser } = require("../services/parser-service");
 const logger = require("../utilities/logger");
 const { createPackingList } = require("../packing-list/index");
 const parserModel = require("../services/parser-model");
-const crypto = require("crypto");
-
-function getRandomInt(min = 1, max = 10000000) {
-  const range = max - min + 1;
-  const randomBuffer = crypto.randomBytes(4); // Get 4 bytes of random data
-  const randomValue = randomBuffer.readUInt32BE(0); // Read an unsigned 32-bit integer from the buffer
-  return (randomValue % range) + min; // Limit to the specified range
-}
+const { getRandomInt } = require("../utilities/random-int");
 
 module.exports = {
   method: "GET",
--- app/routes/non-ai.js
+++ app/routes/non-ai.js
@@ -4,17 +4,10 @@ const { findParser } = require("../services/parser-service");
 const { createPackingList } = require("../packing-list/index");
 const { StatusCodes } = require("http-status-codes");
 const parserModel = require("../services/parser-model");
-const crypto = require("crypto");
 const path = require("path");
 const filenameForLogging = path.join("app", __filename.split("app")[1]);
-
-function getRandomInt(min = 1, max = 10000000) {
-  const range = max - min + 1;
-  const randomBuffer = crypto.randomBytes(4); // Get 4 bytes of random data
-  const randomValue = randomBuffer.readUInt32BE(0); // Read an unsigned 32-bit integer from the buffer
-  return (randomValue % range) + min; // Limit to the specified range
-}
 const logger = require("./../utilities/logger");
+const { getRandomInt } = require("../utilities/random-int");
 
 module.exports = {
   method: "GET",
