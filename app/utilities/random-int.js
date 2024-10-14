ff07bc7e4c8d866347070a98227f4ed304ba1ca7
david-pizzi-cg <david.pizzi@capgemini.com>
2024-10-14 11:20:35 +0100

    Fix CDS Regex for Product header

--- app/utilities/random-int.js
+++ app/utilities/random-int.js
@@ -5,6 +5,6 @@ function getRandomInt(min = 1, max = 10000000) {
   const randomBuffer = crypto.randomBytes(4); // Get 4 bytes of random data
   const randomValue = randomBuffer.readUInt32BE(0); // Read an unsigned 32-bit integer from the buffer
   return (randomValue % range) + min; // Limit to the specified range
-};
+}
 
-module.exports = { getRandomInt }
+module.exports = { getRandomInt };
