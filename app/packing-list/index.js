f49400b11521bbacc4ddc7da07715720c5f3074a
Hanna Sharif-Ali <hanna.sharif-ali@capgemini.com>
2024-11-04 10:07:44 +0000

    correction

--- app/packing-list/index.js
+++ app/packing-list/index.js
@@ -23,7 +23,6 @@ async function createPackingList(packingListJson, applicationId) {
     });
   } catch (err) {
     logger.logError(filenameForLogging, "createPackingList()", err);
-    return err
   }
 }
 
