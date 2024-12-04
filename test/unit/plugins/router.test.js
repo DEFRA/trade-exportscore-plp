f1a92983f645345ee728dea2d2db18d3847dd41a
Padget Avery <padget.avery@capgemini.com>
2024-12-04 14:36:34 +0000

    fix test

--- test/unit/plugins/router.test.js
+++ test/unit/plugins/router.test.js
@@ -3,12 +3,14 @@ const healthyRoutes = require("../../../app/routes/healthy");
 const healthzRoutes = require("../../../app/routes/healthz");
 const aiRoutes = require("../../../app/routes/ai");
 const upsertIdcomsRoutes = require("../../../app/routes/upsert-idcoms");
+const testDiConnRoutes = require("../../../app/routes/test-di-conn");
 const router = require("../../../app/plugins/router");
 
 jest.mock("../../../app/routes/non-ai", () => [{ path: "/non-ai" }]);
 jest.mock("../../../app/routes/healthy", () => [{ path: "/healthy" }]);
 jest.mock("../../../app/routes/healthz", () => [{ path: "/healthz" }]);
 jest.mock("../../../app/routes/ai", () => [{ path: "/ai" }]);
+jest.mock("../../../app/routes/test-di-conn", () => [{ path: "/test-di-conn" }]);
 jest.mock("../../../app/routes/upsert-idcoms", () => [
   { path: "/upsert-idcoms" },
 ]);
@@ -28,6 +30,7 @@ describe("router plugin", () => {
         healthzRoutes,
         upsertIdcomsRoutes,
         aiRoutes,
+        testDiConnRoutes,
       ),
     );
   });
