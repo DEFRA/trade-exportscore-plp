aa0242a980b192d8bf25743ca28efeb0e999bd07
John Fox <john.fox@capgemini.com>
2024-12-17 15:58:02 +0000

    AB#487484 added code to ignore total row at the end of the file

--- test/unit/test-data-and-results/models/asda/model2.js
+++ test/unit/test-data-and-results/models/asda/model2.js
@@ -37,6 +37,41 @@ module.exports = {
       },
     ],
   },
+  validModelWithTotal: {
+    Sheet1: [
+      {
+        B: "[Description Of All Retail Go",
+        D: "[Nature Of Product]",
+        F: "[Treatment Ty",
+        H: "Establishment Number",
+        J: "Cases",
+        L: "Case Weight",
+        N: "NET Weight",
+      },
+      {
+        B: "4PK X 17 PINK LADY APPLES",
+        D: "TOP FRUIT",
+        F: "PRODUCE",
+        H: "RMS-GB-000015-010",
+        J: 20,
+        L: 12.75,
+        N: 255,
+      },
+      {
+        B: "ASDA BABY WATERMELON X10",
+        D: "MELON HARD",
+        F: "PRODUCE",
+        H: "RMS-GB-000015-010",
+        J: 5,
+        L: 12,
+        N: 60,
+      },
+      {
+        L: "TOTAL",
+        N: 1234
+      },
+    ],
+  },
   validHeadersNoData: {
     Sheet1: [
       {
