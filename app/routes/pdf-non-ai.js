684fde9a05c00c3ce591f47d7f9abd8ad2d5b2ea
Padget Avery <padget.avery@capgemini.com>
2025-03-13 16:31:50 +0000

    add booker one page functionality

--- app/routes/pdf-non-ai.js
+++ app/routes/pdf-non-ai.js
@@ -12,6 +12,7 @@ module.exports = {
     const filename = config.plDir + _request.query.filename;
     // First off, we need the RMS number and the starting position of the main table
     let outcome;
+
     try {
       outcome = await pdfExtract.extract(filename);
     } catch (err) {
@@ -20,6 +21,7 @@ module.exports = {
     }
 
     const pages = outcome.pages;
+    console.log(pages)
 
     // Find the RMS number
     let foundRMSElement = null;
@@ -33,7 +35,7 @@ module.exports = {
       }
       if (foundRMSElement) break;
     }
-    console.log(foundRMSElement);
+    //console.log(foundRMSElement);
     const RMS = foundRMSElement.str;
 
     // Find the starting position of the table
@@ -49,27 +51,45 @@ module.exports = {
       }
       if (foundHighestHeaderElement) break;
     }
-    console.log(foundHighestHeaderElement);
+    //console.log(foundHighestHeaderElement);
     const startOfTableYCooord =
       foundHighestHeaderElement.y - foundHighestHeaderElement.height;
     // We now have the pixels of Y of the top of the table
     // We don't bother finding the footer as it's not consistent across the pages
 
-    const pdfParser = new PdfDataParser({
-      url: filename,
-      newlines: false,
-      // repeatingHeaders: true   can't use this as there's repeating headers but with a table above that stops this functionality working
-      pageHeader: startOfTableYCooord,
-      repeatingHeaders: true,
-    });
+    // const pdfParser = new PdfDataParser({
+    //   url: filename,
+    //   newlines: false,
+    //   //orderXY: false,
+    //   // repeatingHeaders: true   can't use this as there's repeating headers but with a table above that stops this functionality working
+    //   pageHeader: startOfTableYCooord,
+    //   repeatingHeaders: true,
+    // });
 
-    const rows = await pdfParser.parse();
+    //const rows = await pdfParser.parse();
 
-    // Rows contains an array of arrays
-    // It's all the pages collapsed together into a large array, we
+    // to be replaced with helper function to find all x and y values
+    const xs = {
+      description: 163,
+      packages: 377.95,
+      weight: 432.91,
+      commodityCode: 507.1
+    }
+    const ys = [217.94, 223.82, 229.7, 235.58, 241.46, 247.34, 253.22, 259.1, 265.01, 270.89, 276.77, 282.65, 288.53, 294.41, 300.29, 306.17, 312.05, 317.93]
 
-    // First off, we need to find the array that contains
+    const packingListContents = [];
 
-    return h.response(rows).code(200);
+    ys.forEach(y => {
+      const plRow = {
+        description: pages[0].content.filter(item => (Math.round(item.y) === Math.round(y)) && (Math.round(item.x) === Math.round(xs.description) && (item.str.trim() !== '')))[0]?.str ?? null,
+        number_of_packages: pages[0].content.filter(item => (Math.round(item.y) === Math.round(y)) && (Math.round(item.x) === Math.round(xs.packages) && (item.str.trim() !== '')))[0]?.str ?? null,
+        total_net_weight_kg: pages[0].content.filter(item => (Math.round(item.y) === Math.round(y)) && (Math.round(item.x) === Math.round(xs.weight) && (item.str.trim() !== '')))[0]?.str ?? null,
+        commodity_code: pages[0].content.filter(item => (Math.round(item.y) === Math.round(y)) && (Math.round(item.x) === Math.round(xs.commodityCode) && (item.str.trim() !== '')))[0]?.str ?? null
+      }
+      packingListContents.push(plRow);
+    })
+
+
+    return h.response(pages).code(200);
   },
 };
