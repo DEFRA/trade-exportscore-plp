f25c0131ebda2708602ceab80f535478fc7180af
Padget Avery <padget.avery@capgemini.com>
2025-05-08 11:54:22 +0100

    undo

--- app/services/parser-map.js
+++ app/services/parser-map.js
@@ -36,12 +36,8 @@ function mapParser(
 ) {
   const headerCols = findHeaderCols(header, packingListJson[headerRow]);
   const netWeightUnit = header.findUnitInHeader
-    ? (regex.findUnit(
-        packingListJson[headerRow][headerCols.total_net_weight_kg],
-      ) ??
-      regex.findUnit(
-        packingListJson[headerRow][headerCols.header_net_weight_unit],
-      ))
+    ? regex.findUnit(packingListJson[headerRow][headerCols.total_net_weight_kg])
+    ??regex.findUnit(packingListJson[headerRow][headerCols.header_net_weight_unit])
     : null;
   const packingListContents = packingListJson
     .slice(dataRow)
