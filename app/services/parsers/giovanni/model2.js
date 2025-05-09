b6b2a3eb05fdf724d736c4494ddb9bfe08c4a399
David Pizzi <81577860+david-pizzi-cg@users.noreply.github.com>
2025-05-09 14:20:44 +0100

    AB#548357 - Add Giovanni 2 Model (#213)

---
+++ app/services/parsers/giovanni/model2.js
@@ -0,0 +1,65 @@
+const combineParser = require("../../parser-combine");
+const parserModel = require("../../parser-model");
+const headers = require("../../model-headers");
+const { mapParser } = require("../../parser-map");
+const regex = require("../../../utilities/regex");
+const { rowFinder } = require("../../../utilities/row-finder");
+const logger = require("../../../utilities/logger");
+const path = require("path");
+const filenameForLogging = path.join("app", __filename.split("app")[1]);
+const { matchesHeader } = require("../../matches-header");
+const MatcherResult = require("../../matcher-result");
+
+function parse(packingListJson) {
+  try {
+    const sheets = Object.keys(packingListJson);
+    let packingListContents = [];
+    let packingListContentsTemp = [];
+    const headerTitles = Object.values(headers.GIOVANNI2.regex);
+    const callback = function (x) {
+      return matchesHeader(headerTitles, [x]) === MatcherResult.CORRECT;
+    };
+    const headerRow = rowFinder(packingListJson[sheets[0]], callback);
+    const dataRow = headerRow + 1;
+
+    const establishmentNumber = regex.findMatch(
+      headers.GIOVANNI2.establishmentNumber.regex,
+      packingListJson[sheets[0]],
+    );
+
+    for (const sheet of sheets) {
+      packingListContentsTemp = mapParser(
+        packingListJson[sheet],
+        headerRow,
+        dataRow,
+        headers.GIOVANNI2,
+        sheet,
+      );
+      packingListContents = packingListContents.concat(packingListContentsTemp);
+    }
+
+    packingListContents = packingListContents.filter(
+      (row) =>
+        !(
+          row.description === "(blank)" &&
+          row.commodity_code === "(blank)" &&
+          row.number_of_packages === 0 &&
+          row.total_net_weight_kg === 0
+        ),
+    );
+
+    return combineParser.combine(
+      establishmentNumber,
+      packingListContents,
+      true,
+      parserModel.GIOVANNI2,
+    );
+  } catch (err) {
+    logger.logError(filenameForLogging, "matches()", err);
+    return combineParser.combine(null, [], false, parserModel.NOMATCH);
+  }
+}
+
+module.exports = {
+  parse,
+};
