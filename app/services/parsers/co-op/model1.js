286fe799fb54d269e6a56f6eb275356da7977793
David Pizzi <david.pizzi@capgemini.com>
2025-06-10 10:05:15 +0100

    Co-op single rms

--- app/services/parsers/co-op/model1.js
+++ app/services/parsers/co-op/model1.js
@@ -15,12 +15,20 @@ function parse(packingListJson) {
     const sheets = Object.keys(packingListJson);
     let packingListContents = [];
     let packingListContentsTemp = [];
+    let establishmentNumbers = [];
+
     const establishmentNumber = regex.findMatch(
       headers.COOP1.establishmentNumber.regex,
       packingListJson[sheets[0]],
     );
 
     for (const sheet of sheets) {
+      establishmentNumbers = regex.findAllMatches(
+        regex.remosRegex,
+        packingListJson[sheet],
+        establishmentNumbers,
+      );
+
       const headerTitles = Object.values(headers.COOP1.regex);
       const headerCallback = function (x) {
         return matchesHeader(headerTitles, [x]) === MatcherResult.CORRECT;
@@ -43,6 +51,7 @@ function parse(packingListJson) {
       packingListContents,
       true,
       parserModel.COOP1,
+      establishmentNumbers,
     );
   } catch (err) {
     logger.logError(filenameForLogging, "matches()", err);
