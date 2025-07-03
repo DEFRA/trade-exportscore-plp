10331674a398879143e502ad170ce64092fef95d
John Fox <john.fox@capgemini.com>
2025-07-03 11:27:13 +0100

    AB#570124 BandM Single RMS

--- test/unit/test-data-and-results/results/bandm/model1.js
+++ test/unit/test-data-and-results/results/bandm/model1.js
@@ -91,4 +91,44 @@ module.exports = {
     registration_approval_number: null,
     parserModel: parserModel.BANDM1,
   },
+  multipleRms: {
+    business_checks: {
+      all_required_fields_present: false,
+      failure_reasons: 'Multiple GB Place of Dispatch (Establishment) numbers found on packing list.\n',
+    },
+    items: [
+      {
+        commodity_code: 16025095,
+        description: "J/L JERKY 70G TERIYAKI",
+        nature_of_products: null,
+        number_of_packages: 1,
+        total_net_weight_kg: 1.15,
+        type_of_treatment: null,
+        total_net_weight_unit: "KG",
+      }
+    ],
+    establishment_numbers: ['RMS-GB-000005-001', 'RMS-GB-000005-002'],
+    registration_approval_number: "RMS-GB-000005-001",
+    parserModel: parserModel.BANDM1,
+  },
+  missingKgunit: {
+    business_checks: {
+      all_required_fields_present: false,
+      failure_reasons: 'Net Weight Unit of Measure (kg) not found.\n',
+    },
+    items: [
+      {
+        commodity_code: 16025095,
+        description: "J/L JERKY 70G TERIYAKI",
+        nature_of_products: null,
+        number_of_packages: 1,
+        total_net_weight_kg: 1.15,
+        type_of_treatment: null,
+        total_net_weight_unit: null,
+      }
+    ],
+     establishment_numbers: ['RMS-GB-000005-001'],
+    registration_approval_number: "RMS-GB-000005-001",
+    parserModel: parserModel.BANDM1,
+  },
 };
