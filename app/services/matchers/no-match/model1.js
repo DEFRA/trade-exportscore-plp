a2b9bb8d0db5680fb8c346ef3eda8df698ed3094
Padget Avery <padget.avery@capgemini.com>
2025-07-15 11:08:47 +0100

    sonar

--- app/services/matchers/no-match/model1.js
+++ app/services/matchers/no-match/model1.js
@@ -6,25 +6,12 @@ const filenameForLogging = path.join("app", __filename.split("app")[1]);
 
 function noRemosMatch(sanitisedPackingList, _filename) {
   const remosRegex = /^RMS-GB-\d{6}-\d{3}$/i;
-  const giovanni2Regex = /\(NIRMS RMS-GB-\d{6}-\d{3}\)/i;
-  const cdsRegex = /\/ RMS-GB-000252-\d{3} \//i;
-  const sainsburysRegex = /^RMS-GB-000094-\d{3}​$/i;
-  const giovanni1Regex = /^RMS-GB-000153$/i;
-  const kepakRegex = /^RMS-GB-000280$/i;
-  const nutriciaRegex = /^RMS-GB-000133$/i;
+
   const sheets = Object.keys(sanitisedPackingList);
   for (const sheet of sheets) {
     const isRemosPresent = sanitisedPackingList[sheet].some((x) => {
       return Object.values(x).some((y) => {
-        return (
-          remosRegex.test(y) ||
-          giovanni2Regex.test(y) ||
-          cdsRegex.test(y) ||
-          sainsburysRegex.test(y) ||
-          giovanni1Regex.test(y) ||
-          kepakRegex.test(y) ||
-          nutriciaRegex.test(y)
-        );
+        return remosRegex.test(y) || rmsExceptions(y);
       });
     });
     if (isRemosPresent) {
@@ -32,6 +19,24 @@ function noRemosMatch(sanitisedPackingList, _filename) {
     }
   }
   return false;
+}
+
+function rmsExceptions(y) {
+  const giovanni2Regex = /\(NIRMS RMS-GB-\d{6}-\d{3}\)/i;
+  const cdsRegex = /\/ RMS-GB-000252-\d{3} \//i;
+  const sainsburysRegex = /^RMS-GB-000094-\d{3}​$/i;
+  const giovanni1Regex = /^RMS-GB-000153$/i;
+  const kepakRegex = /^RMS-GB-000280$/i;
+  const nutriciaRegex = /^RMS-GB-000133$/i;
+
+  return (
+    giovanni2Regex.test(y) ||
+    cdsRegex.test(y) ||
+    sainsburysRegex.test(y) ||
+    giovanni1Regex.test(y) ||
+    kepakRegex.test(y) ||
+    nutriciaRegex.test(y)
+  );
 }
 
 async function noRemosMatchPdf(packingList) {
