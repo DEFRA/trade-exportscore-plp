0b6586289e1c52f8df6fc2aa9e777be20717efbf
Padget Avery <padget.avery@capgemini.com>
2025-08-07 11:29:48 +0100

    iceland tests

--- app/services/parsers/iceland/model1.js
+++ app/services/parsers/iceland/model1.js
@@ -6,10 +6,16 @@ const headers = require("../../model-headers-pdf");
 const regex = require("../../../utilities/regex");
 const path = require("path");
 const filenameForLogging = path.join("app", __filename.split("app")[1]);
+const {
+  extractPdf,
+  extractEstablishmentNumbersFromString,
+} = require("../../../utilities/pdf-helper");
 
-function parse(packingListDocument) {
+async function parse(packingListDocument, sanitizedFullPackingList) {
   try {
     let establishmentNumber;
+    let establishmentNumbers = [];
+
     if (
       regex.findMatch(headers.ICELAND1.establishmentNumber.regex, [
         packingListDocument.fields.PartialNIRMSNumber,
@@ -19,12 +25,20 @@ function parse(packingListDocument) {
     }
     const packingListContents = mapPdfParser(packingListDocument, "ICELAND1");
 
+    if (!!sanitizedFullPackingList) {
+      const pdfJson = await extractPdf(sanitizedFullPackingList);
+      establishmentNumbers = extractEstablishmentNumbersFromString(
+        pdfJson,
+        headers.ICELAND1.establishmentNumber.establishmentRegex,
+      );
+    }
+
     return combineParser.combine(
       establishmentNumber,
       packingListContents,
       true,
       parserModel.ICELAND1,
-      [],
+      establishmentNumbers,
       headers.ICELAND1.findUnitInHeader,
     );
   } catch (err) {
