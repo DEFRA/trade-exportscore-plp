f6558f8211c52b85b18f7b8e00eb68879814c8b3
Hanna Sharif-Ali <hanna.sharif-ali@capgemini.com>
2025-08-28 15:30:53 +0100

    AB#591535: Feature/591535 nisa1 coo (#351)
    * nisa1 CoO and tests
    
    * 6.21.3
    
    * made changes to test data
    
    * test data updated

--- test/unit/services/parser-service/nisa/model1.test.js
+++ test/unit/services/parser-service/nisa/model1.test.js
@@ -2,9 +2,21 @@ const parserService = require("../../../../../app/services/parser-service");
 const model = require("../../../test-data-and-results/models/nisa/model1");
 const parserModel = require("../../../../../app/services/parser-model");
 const test_results = require("../../../test-data-and-results/results/nisa/model1");
+const failureReasons = require("../../../../../app/services/validators/packing-list-failure-reasons");
+
+jest.mock("../../../../../app/services/data/data-iso-codes.json", () => [
+  "VALID_ISO",
+  "PROHIBITED_ITEM_ISO",
+]);
+jest.mock("../../../../../app/services/data/data-prohibited-items.json", () => [
+  {
+    country_of_origin: "PROHIBITED_ITEM_ISO",
+    commodity_code: "012",
+    type_of_treatment: "PROHIBITED_ITEM_TREATMENT",
+  },
+]);
 
 const filename = "packinglist-nisa-model1.xlsx";
-
 describe("findParser", () => {
   test("matches valid Nisa Model 1 file, calls parser and returns all_required_fields_present as true", async () => {
     const result = await parserService.findParser(model.validModel, filename);
@@ -51,5 +63,62 @@ describe("findParser", () => {
     );
 
     expect(result).toMatchObject(test_results.missingKgunit);
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as false for invalid NIRMS", async () => {
+    const result = await parserService.findParser(model.invalidNirms, filename);
+
+    expect(result.business_checks.failure_reasons).toBe(
+      failureReasons.NIRMS_INVALID + ' in sheet "Customer Order" row 2.\n',
+    );
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as true for valid NIRMS", async () => {
+    const result = await parserService.findParser(model.nonNirms, filename);
+
+    expect(result.business_checks.all_required_fields_present).toBeTruthy();
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as false for missing NIRMS", async () => {
+    const result = await parserService.findParser(model.missingNirms, filename);
+
+    expect(result.business_checks.failure_reasons).toBe(
+      failureReasons.NIRMS_MISSING + ' in sheet "Customer Order" row 2.\n',
+    );
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as false for missing CoO", async () => {
+    const result = await parserService.findParser(model.missingCoO, filename);
+
+    expect(result.business_checks.failure_reasons).toBe(
+      failureReasons.COO_MISSING +
+        ' in sheet "Customer Order" row 2 and sheet "Customer Order" row 3.\n',
+    );
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as false for invalid CoO", async () => {
+    const result = await parserService.findParser(model.invalidCoO, filename);
+
+    expect(result.business_checks.failure_reasons).toBe(
+      failureReasons.COO_INVALID +
+        ' in sheet "Customer Order" row 2 and sheet "Customer Order" row 3.\n',
+    );
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as true for X CoO", async () => {
+    const result = await parserService.findParser(model.xCoO, filename);
+
+    expect(result.business_checks.all_required_fields_present).toBeTruthy();
+  });
+
+  test("matches valid NISA Model 1 file, calls parser and returns all_required_fields_present as false for prohibited items", async () => {
+    const result = await parserService.findParser(
+      model.prohibitedItems,
+      filename,
+    );
+
+    expect(result.business_checks.failure_reasons).toBe(
+      failureReasons.PROHIBITED_ITEM + ' in sheet "Customer Order" row 2 and sheet "Customer Order" row 4.\n',
+    );
   });
 });
