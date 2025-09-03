289ce6a6ad5621ba9e1cb252db29e6d607982b04
John Fox <john.fox@capgemini.com>
2025-09-03 14:17:36 +0100

    Initial commit of Booker2 model and prompt sto create a parser and document it

---
+++ app/services/parsers/booker/model2.js
@@ -0,0 +1,76 @@
+const combineParser = require("../../parser-combine");
+const parserModel = require("../../parser-model");
+const headers = require("../../model-headers");
+const { rowFinder } = require("../../../utilities/row-finder");
+const { mapParser } = require("../../parser-map");
+const { matchesHeader } = require("../../matches-header");
+const MatcherResult = require("../../matcher-result");
+const regex = require("../../../utilities/regex");
+const logger = require("../../../utilities/logger");
+const path = require("path");
+const filenameForLogging = path.join("app", __filename.split("app")[1]);
+
+function parse(packingListJson) {
+  try {
+    const sheets = Object.keys(packingListJson);
+    let packingListContents = [];
+    let packingListContentsTemp = [];
+    let establishmentNumbers = [];
+
+    const establishmentNumber = regex.findMatch(
+      headers.BOOKER2.establishmentNumber.regex,
+      packingListJson[sheets[0]],
+    );
+
+    const footerValues = [/^TOTALS$/i];
+    const callback = function (x) {
+      return regex.testAllPatterns(footerValues, x);
+    };
+
+    for (const sheet of sheets) {
+      establishmentNumbers = regex.findAllMatches(
+        regex.remosRegex,
+        packingListJson[sheet],
+        establishmentNumbers,
+      );
+
+      const footerRow = rowFinder(packingListJson[sheet], callback);
+      if (footerRow !== -1) {
+        packingListJson[sheet] = packingListJson[sheet].slice(0, footerRow);
+      }
+
+      const headerTitles = Object.values(headers.BOOKER2.regex);
+      const headerCallback = function (x) {
+        return matchesHeader(headerTitles, [x]) === MatcherResult.CORRECT;
+      };
+
+      const headerRow = rowFinder(packingListJson[sheets[0]], headerCallback);
+      const dataRow = headerRow + 1;
+
+      packingListContentsTemp = mapParser(
+        packingListJson[sheet],
+        headerRow,
+        dataRow,
+        headers.BOOKER2,
+        sheet,
+      );
+      packingListContents = packingListContents.concat(packingListContentsTemp);
+    }
+
+    return combineParser.combine(
+      establishmentNumber,
+      packingListContents,
+      true,
+      parserModel.BOOKER2,
+      establishmentNumbers,
+      headers.BOOKER2.findUnitInHeader,
+    );
+  } catch (err) {
+    logger.logError(filenameForLogging, "parse()", err);
+    return combineParser.combine(null, [], false, parserModel.NOMATCH, []);
+  }
+}
+
+module.exports = {
+  parse,
+};
