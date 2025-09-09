f3eb36b9f5d4553aab7a1c2efff9d17f520029bf
Hanna Sharif-Ali <hanna.sharif-ali@capgemini.com>
2025-09-09 13:19:48 +0100

    correction

--- app/services/parsers/savers/model1.js
+++ app/services/parsers/savers/model1.js
@@ -75,8 +75,7 @@ function parse(packingListJson) {
       false,
       parserModel.SAVERS1,
       [],
-      headers.SAVERS1.findUnitInHeader,
-      headers.SAVERS1.validateCountryOfOrigin,
+      headers.SAVERS1,
     );
   }
 }
