209ef3a2557e02a2c51365fa64b913d4ca330d5b
deanfmbrowncapgemini <dean.fm.brown@capgemini.com>
2025-10-30 17:06:09 +0000

    Adjusting Tesco 2 to consider Country of Origin

--- app/services/model-headers.js
+++ app/services/model-headers.js
@@ -409,11 +409,16 @@ const headers = {
     },
     regex: {
       description: descriptionOfGoodsRegex,
-      commodity_code: commodityCodeRegex,
+      commodity_code: /Commodity code/i,
       number_of_packages: noOfPackagesRegex,
       total_net_weight_kg: /Total Net Weight/i,
+      nature_of_products: /Nature of Product \(Chilled \/Ambient\/ Frozen\)/i,
+      type_of_treatment: /Type of Treatment \(Processed\/ Unprocessed\/ Raw\)/i,
     },
     findUnitInHeader: true,
+    country_of_origin: /Country of Origin/i,
+    nirms: /NIRMS \/ Non NIRMS/i,
+    validateCountryOfOrigin: true,
   },
   TESCO3: {
     establishmentNumber: {
