e2bc0a3976935e223f67e4c58dd4e2abc6595106
hanna1013 <hanna.sharif-ali@capgemini.com>
2025-11-03 17:18:19 +0000

    passed in regex

--- app/services/matchers/fowlerwelch/model2.js
+++ app/services/matchers/fowlerwelch/model2.js
@@ -1,7 +1,11 @@
 const { matchesModel } = require("../warrens/model2");
-
+const headers = require("../../model-headers");
 function matches(packingList, filename) {
-  return matchesModel(packingList, filename, /^RMS-GB-000216-\d{3}$/);
+  return matchesModel(
+    packingList,
+    filename,
+    headers.FOWLERWELCH2.establishmentNumber.regex,
+  );
 }
 
 module.exports = { matches };
